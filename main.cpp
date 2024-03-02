ce25d57bdf2b8d201b94d64219e3872941c1a1d5
Jgor2001 <igor-lapshin-2013@bk.ru>
2024-03-02 16:06:43 +0700

    adding class Test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+class Test
+{
+
+public :
+    int Info;
+    Test(int inf);
+
+};
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
